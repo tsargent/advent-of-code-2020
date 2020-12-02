78becead54e38925119d2fbbbcc854e8da0c8504
Tyler Sargent <tsargent@gmail.com>
2020-12-02 13:45:37 -0500

    day 02 part 2

--- src/day-02/01.js
+++ src/day-02/01.js
@@ -4,15 +4,18 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
   let lines = data.split('\n');
   const arr = [];
   let validCount = 0;
+  let validCount2 = 0;
   lines.forEach((line) => {
     if (line) {
       const parts = line.split(':');
       const rule = parts[0].trim();
       const str = parts[1].trim();
       isValid(rule, str) && validCount++;
+      isValid2(rule, str) && validCount2++;
     }
   });
   console.log(validCount);
+  console.log(validCount2);
 });
 
 function isValid(rule, str) {
@@ -22,4 +25,19 @@ function isValid(rule, str) {
   const valid = occurances >= ruleRange[0] && occurances <= ruleRange[1];
 
   return valid;
+}
+
+function isValid2(rule, str) {
+  const [pos1, pos2] = rule.split(' ')[0].split('-').map((str => Number(str)));
+  const ruleValue = rule.split(' ')[1];
+
+  if (str[pos1 - 1] === ruleValue && str[pos2 - 1] === ruleValue) {
+    return false;
+  }
+
+  if (str[pos1 - 1] !== ruleValue && str[pos2 - 1] !== ruleValue) {
+    return false;
+  }
+
+  return true;
 }
