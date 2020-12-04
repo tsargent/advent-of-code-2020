aa8610d2600145f1cfea314f79870167a782c2f8
Tyler Sargent <tsargent@gmail.com>
2020-12-03 22:23:52 -0500

    day 03 part 2

--- src/day-03/01.js
+++ src/day-03/01.js
@@ -2,12 +2,23 @@ const fs = require('fs');
 const data = fs.readFileSync('./input.txt');
 const dataString = data.toString().trim();
 const arr = dataString.split('\n').map((line) => line.split(''));
-let treeCount = 0;
 const columns = arr[0].length;
-for (let i = 0; i < arr.length; i++) {
-  const col = (i * 3) % columns;
-  const isTree = arr[i][col] === '#';
-  isTree && treeCount++;
+
+function countTrees(right, down) {
+  let treeCount = 0;
+  for (let i = 0; i < (arr.length / down); i++) {
+    const col = (i * right) % columns;
+    const row = i * down;
+    const isTree = arr[row][col] === '#';
+    isTree && treeCount++;
+  }
+  return treeCount;
 }
 
-console.log(treeCount);
+const answ1 = countTrees(1, 1);
+const answ2 = countTrees(3, 1);
+const answ3 = countTrees(5, 1);
+const answ4 = countTrees(7, 1);
+const answ5 = countTrees(1, 2);
+
+console.log(answ1*answ2*answ3*answ4*answ5)
