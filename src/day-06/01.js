efc3e75fd9903e9a590cd4f8bb49db8e5e718f18
Tyler Sargent <tsargent@gmail.com>
2020-12-06 08:35:24 -0500

    day 06 part 1

---
+++ src/day-06/01.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const data = fs.readFileSync("./input.txt");
+const dataString = data.toString().trim();
+
+// get total "yes" for each group
+const results = dataString.split('\n\n').map((group) => {
+  const line = group.replace(/(\r\n|\n|\r)/gm, "");
+  const uniqueOnly = new Set(line);
+  return [...uniqueOnly].length;
+});
+
+// get the sum
+const sum = results.reduce((a, c) => a + c, 0);
+
+console.log(sum)
