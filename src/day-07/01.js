8fff999c84d050b0fbe929989afe2e56546a28c1
Tyler Sargent <tsargent@gmail.com>
2020-12-08 09:17:10 -0500

    day 07 cleanup

--- src/day-07/01.js
+++ src/day-07/01.js
@@ -1,17 +1,7 @@
 const fs = require("fs");
 const data = fs.readFileSync("./input.txt");
 const dataString = data.toString().trim();
-
 const dataLines = dataString.split("\n");
-
-const bagColors = new Set();
-dataLines.forEach((line) => {
-  const [key] = line.split("contain");
-  bagColors.add(key);
-  if (key.includes("gold")) {
-    // console.log(key);
-  }
-});
 
 function makeValue(str) {
   const sliceIndex = str.indexOf(" ");
@@ -64,24 +54,3 @@ function findAllParents(obj, children) {
 }
 
 console.log(findAllParents(masterObject, ["shiny-gold"]));
-// console.log(findAllParents(masterObject, ['fake-color']));
-// // 1 get the bags that directly contain shiny gold bags
-// const parents = new Set();
-
-// for (key in masterObject) {
-//   if ('shiny-gold' in masterObject[key]) {
-//     parents.add(key);
-//   }
-// }
-
-// console.log('These bags directly contain shiny gold:');
-// console.log(parents);
-// // console.log(dataLines.length);
-// // console.log([...bagColors].length)
-
-// const grandParents = new Set();
-
-// parents.forEach((el) => {
-//   console.log(el)
-
-// })
